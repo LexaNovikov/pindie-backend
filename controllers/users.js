784a4e3d83b094e8f227037eff529d2bf5402d84
LexaNovikov <lexaproxa228@gmail.com>
2024-05-24 18:28:39 +0300

    готовый бэк

--- controllers/users.js
+++ controllers/users.js
@@ -3,4 +3,34 @@ const sendAllUsers = (req, res) => {
     res.end(JSON.stringify(req.usersArray));
 };
 
-module.exports = sendAllUsers;
+const sendCreatedUser = (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(req.user));
+}
+
+const sendUserById = (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(req.user));
+};
+
+const sendUserUpdated = (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(JSON.stringify({ message: "Пользователь обновлен" }))
+};
+const sendUserDeleted = (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(req.user))
+}
+
+const sendMe = (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(req.user));
+}
+module.exports = {
+    sendAllUsers,
+    sendCreatedUser,
+    sendUserById,
+    sendUserUpdated,
+    sendUserDeleted,
+    sendMe
+};
